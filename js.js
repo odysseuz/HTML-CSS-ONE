bd4db46cd70732ded1aaa2f6373e7813f5366961
Ulises Ramos <784882@seq.org>
2016-05-27 10:52:31 -0700

    Add files via upload

--- js.js
+++ js.js
@@ -1,42 +1,50 @@
 
-var MAX_NUMBERS=0;
-var nums =[];
 
-  for(var i = 0; i < MAX_NUMBERS; i++){
+
+var MAX_NUMBERS=10;
+
+var nums=[];
+for(var i=0; i<MAX_NUMBERS;i++)
+{
   nums[i]= Math.floor(Math.random()*10);
-  }
-
-
+}
 
 function getValue(){
-nums.sort();
-  document.write(nums);
+  nums.sort();
+  document.getElementById('out').innerHTML += '<br>Numbas== ' + (nums)
 
 }
 function Average(){
+  nums.sort();
   var total = 0;
   var length = 0;
+
   for(var i = 0; i < nums.length; i++){
     total += nums[i];
     console.log(nums);
+  }
+  length = total/nums.length
+  document.getElementById('out').innerHTML += '<br>Mean== ' + (length);
+}
+
+function median() {
+
+  var half = Math.floor(nums.length/2);
+  //document.write(half);
+  if(nums.length % 2)
+  {
+    document.getElementById('out').innerHTML += '<br>Median== ' + (nums[half]);
 
   }
-  length = total/nums.length;
-  document.write(length);
-}
-function median(){
-  var half = Math.floor(nums.length/2);
-  if(nums.length % 2){
-    document.write(nums[half]);
-  }
   else{
-    document write(nums[half-1] + nums[half] / 2.0);
+    document.getElementById('out').innerHTML += '<br>Median== ' + ((nums[half-1] + nums[half]) / 2.0);
   }
 }
 function mode(){
+
   var numMapping = {};
   for(var i = 0; i < nums.length; i++){
-    if(numMapping[nums[i]] == undefined){
+    if(numMapping[nums[i]] === undefined){
       numMapping[nums[i]] = 0;
     }
     numMapping[nums[i]] += 1;
@@ -49,5 +57,7 @@ function mode(){
       mode = prop;
     }
   }
-  document.write(parseInt(mode));
+  document.getElementById('out').innerHTML += '<br>Mode== ' + (parseInt(mode));
 }
+
+//console.log(getValue());
